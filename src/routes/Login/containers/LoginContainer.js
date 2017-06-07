f3ff8c539d94c0d4a1c0dd61067059996ecd9107
roslanelyashiv <roslane@shellanoo.com>
2017-06-07 18:31:01 +0300

    finished redux and starting login

--- src/routes/Login/containers/LoginContainer.js
+++ src/routes/Login/containers/LoginContainer.js
@@ -4,6 +4,24 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 class LoginContainer extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      credentials: {
+        email: '',
+        password: ''
+      },
+    }
+    this.onChange = this.onChange.bind(this)
+    this.onSave = this.onSave.bind(this)
+  }
+
+  onChange () {
+    const field = event.target.name
+    const credentials = this.state.credentials
+    credentials[field] = event.target.valueOf()
+    return this.setState({ credentials: credentials })
+  }
   render () {
     return (
       <div className='wrap-login'>
